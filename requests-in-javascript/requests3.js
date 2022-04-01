e2cb0859b7dc6e46558d2ab55b9bdeb9765e1673
Molly <mojonesycreative@gmail.com>
2022-04-01 17:57:43 -0400

    testing promise.allSettled, console status

--- requests-in-javascript/requests3.js
+++ requests-in-javascript/requests3.js
@@ -1,41 +1,47 @@
 const axios = require("axios");
 const BASE_URL = "http://localhost:5000";
 
-// Promise.all([
-//   axios.get(`${BASE_URL}/constellations/UEUrlfX`),
-//   axios.get(`${BASE_URL}/constellations/zb8QvVt`),
-//   axios.get(`${BASE_URL}/constellations/32TN5F8`),
-// ]).then((results) => {
-//     console.log(results[0].data);
-//     console.log(results[1].data);
-//     console.log(results[2].data);
+
+Promise.allSettled([
+  axios.get(`${BASE_URL}/constellations/UEUrlfX`),
+  axios.get(`${BASE_URL}/constellations/zb8QvVt`),
+  axios.get(`${BASE_URL}/constellations/32TN5F8`),
+]).then((results) => {
+    console.log(results[0].status);
+    console.log(results[1].status);
+    console.log(results[2].status);
+  })
+  .catch(error => console.log(`${error}`));
+
+
+
+
+// const ids = ["KGQIwSq", "32TN5F8"];
+// getConstellations(ids).then((results) => {
+//   console.log(results[0].data);
+//   console.log(results[1].data);
+// });
+
+// function getConstellations(ids) {
+//   const promises = ids.map((id) => {
+//     const url = `${BASE_URL}/constellations/${id}`;
+//     return axios.delete(url);
 //   });
 
-
-  // function getConstellations(ids) {
-  //   const promises = ids.map((id) => {
-  //     const url = `${BASE_URL}/constellations/${id}`;
-  //     return axios.delete(url);
-  //   });
+//   return Promise.all(promises);
+// }
+// getConstellations(ids).then(console.log);
   
-  //   return Promise.all(promises);
+
+
+  // async function getConstellationNameById(id) {
+  //   const url = `${BASE_URL}/constellations/${id}`;
+  //   try {
+  //     const { data } = await axios.get(url);
+  //     return data;
+  //   } catch (error) {
+  //     throw `Constellation with id of ${id} could not be found.`;
+  //   }
   // }
-  
-  // const ids = ["KGQIwSq", "32TN5F8"];
-  // getConstellations(ids).then((results) => {
-  //     console.log(results[0].data);
-  //     console.log(results[1].data);
-  // });
 
-  
-  async function getConstellationNameById(id) {
-    const url = `${BASE_URL}/constellations/${id}`;
-    try {
-      const { data } = await axios.get(url);
-      return data;
-    } catch (error) {
-      throw `Constellation with id of ${id} could not be found.`;
-    }
-  }
-
-  getConstellationNameById("n2OEOzp").then(console.log);
+  // getConstellationNameById("n2OEOzp").then(console.log);
